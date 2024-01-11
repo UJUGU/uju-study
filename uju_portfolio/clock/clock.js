4f6d174c38fb87107ac63f0b0704a97a83ba832d
ujugu <ujugu@org-i.com>
2024-01-11 17:02:48 +0900

    clock, todolist 스터디

---
+++ uju_portfolio/clock/clock.js
@@ -0,0 +1,17 @@
+const clockContainer = document.querySelector('.js-clock');
+const clockTitle = clockContainer.querySelector("h1");
+
+function getTime() {
+    const date = new Date();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    const hours = date.getHours();
+
+    clockTitle.innerText = `${hours}:${minutes}:${seconds ``}`;
+}
+
+function init(){
+    getTime();
+    setInterval(getTime, 1000);
+}
+init();
