9453c1dee841757889db9a541e10f5c9ba866ca0
ujugu <ujugu@org-i.com>
2024-01-11 21:31:25 +0900

    todolist 추가작업

--- uju_portfolio/todolist/todolist.js
+++ uju_portfolio/todolist/todolist.js
@@ -8,5 +8,23 @@ function addItem() {
     //li 요소에 들어갈 닫기 버튼 생성
     let xbtn = document.createElement("button");
 
+    listitem.className = 'tag is-success is-light is-medium block';
+    xbtn. className = 'delete is-small';
 
+    //close 버튼에 이벤트 처리
+    xbtn.onclick = function(e) {
+        //이벤트가 발생한 li 요소를 구해서 ul 요소에서 제거
+        let pnode = e.target.parentNode;
+        list.removeChild(pnode);
+    }
+    //li 요소 구성
+    listitem.innerText = todo.value;
+    listitem.appendChild(xbtn);
+    
+    //ul 요소에 li 요소 추가
+    list.appendChild(listitem);
+
+    //입력칸 비우기 및 포커스 이동
+    todo.value = '';
+    todo.focus();
 }
